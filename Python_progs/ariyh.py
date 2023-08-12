192461c95fc331ad877a3f3d068c1d6c5b922424
Ranjan.B <rockytomy20>
2023-08-12 11:56:09 +0530

    subtraction added to the file

--- Python_progs/ariyh.py
+++ Python_progs/ariyh.py
@@ -1,5 +1,14 @@
 def add(n1,n2):
     return(n1+n2)
+print('for Division')
 a=int(input("Enter num1:"))
 b=int(input("Enter num2:"))
 print("sum=",add(a,b))
+
+def sub(n1,n2):
+    return(n1-n2)
+print('for subtraction')
+a=int(input("Enter num1:"))
+b=int(input("Enter num2:"))
+print("sub=",add(a,b))
+
