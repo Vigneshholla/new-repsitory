6c0da934a46d0d60da1e619ac2acf16d62dcee81
Vigneshholla <134056031+Vigneshholla@users.noreply.github.com>
2023-08-12 12:27:33 +0530

    Update ariyh.py
    redo

--- Python_progs/ariyh.py
+++ Python_progs/ariyh.py
@@ -5,10 +5,5 @@ a=int(input("Enter num1:"))
 b=int(input("Enter num2:"))
 print("sum=",add(a,b))
 
-def sub(n1,n2):
-    return(n1-n2)
-print('for subtraction')
-a=int(input("Enter num1:"))
-b=int(input("Enter num2:"))
-print("sub=",add(a,b))
+
 
